03fc322807b2891cccb54f5c900435f9c3aebf07
NosxyLw <gulain77@gmail.com>
2025-11-27 23:36:43 +0100

    Delete js directory

--- js/common.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    const header = document.querySelector('header');
-    function onScrollHeader(){ 
-        if(window.scrollY > 50) header.classList.add('scrolled'); 
-        else header.classList.remove('scrolled'); 
-    }
-    window.addEventListener('scroll', onScrollHeader, { passive: true }); 
-    onScrollHeader();
-})();
